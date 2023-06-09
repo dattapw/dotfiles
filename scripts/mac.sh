7ea2b9fc6a6df73a5cdc6fcd5e99c688b9ed6c69
Saurabh Datta <contact@saurabhdatta.com>
2023-06-09 17:22:58 +0530

    Update mac.sh

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -18,18 +18,8 @@ defaults write com.apple.terminal StringEncodings -array 4
 # echo "show the ~/Library folder in Finder"
 chflags nohidden ~/Library
 
-echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-echo "Enable subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
-
 echo "Enable automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
-
-echo "Enable Show remaining battery time;"
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-defaults write com.apple.menuextra.battery ShowTime -string "NO"
 
 echo "Use current directory as default search scope in Finder"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
@@ -40,18 +30,9 @@ defaults write com.apple.finder ShowPathbar -bool true
 echo "Show Status bar in Finder"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 1
-
-echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-
 echo "disabling smart quotes and dashes..."
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-echo "Set date display in menu bar"
-defaults write com.apple.menuextra.clock "DateFormat" "EEE MMM d  H.mm"
 
 echo "Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -60,24 +41,6 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 echo "Expand print panel by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-echo "Disable the “Are you sure you want to open this application?” dialog"
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-echo "Disable smart quotes as they’re annoying when typing code"
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-echo "Stop iTunes from responding to the keyboard media keys"
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
-
-echo "Hide all desktop icons because who need 'em'"
-defaults write com.apple.finder CreateDesktop -bool false
-
-echo "Remove delay when taking a screenshot"
-defaults write com.apple.screencapture show-thumbnail -bool false
-
-echo "Store screenshots in /tmp"
-defaults write com.apple.screencapture location /tmp
 
 echo "Hide 'recent applications' from dock"
 defaults write com.apple.dock show-recents -bool false
@@ -88,21 +51,9 @@ defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreS
 echo "Only show active apps in dock"
 defaults write com.apple.dock static-only -bool true; killall Dock
 
-echo "Disable CMD+space for spotlight"
-/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist -c "Set AppleSymbolicHotKeys:64:enabled false"
-
-echo "Always show scrollbars"
-defaults write -g AppleShowScrollBars -string "Always"
-
-
 echo "Speed up dock show/hide"
 defaults write com.apple.dock autohide-delay -float 0;
 defaults write com.apple.dock autohide-time-modifier -int 0;
-
-echo "Set red accent and highlight color"
-# more info https://github.com/yuhonas/osx-colors/blob/main/src/color_utils.py
-defaults write -g AppleAccentColor -string 0
-defaults write -g AppleHighlightColor -string "1.000000 0.733333 0.721569 Red"
 
 echo "Update Apple developer utils"
 softwareupdate --all --install --force
